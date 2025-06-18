defe5b38b3eaa8b9826bd7d53971dd767b803049
Yann from dev machine <yann@lysden.art>
2025-06-18 09:51:13 +0200

    Remove BugSnag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,15 @@ import 'react-color-picker/index.css'
 import '@reach/combobox/styles.css'
 
 import { wrapper } from '~core/store'
-import { ErrorBoundary as BugsnagErrorBoundary } from '~utils/bugsnag'
 import AppErrorBoundary from '~components/errorBoundaries/AppErrorBoundary'
 import { AppProps } from 'next/app'
 
 const Main = ({ Component, pageProps }: AppProps) => (
-  <BugsnagErrorBoundary>
     <ChakraProvider resetCSS theme={theme}>
       <AppErrorBoundary>
         <Component {...pageProps} />
       </AppErrorBoundary>
     </ChakraProvider>
-  </BugsnagErrorBoundary>
 )
 
 export default wrapper.withRedux(Main)
